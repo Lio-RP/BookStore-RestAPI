36f5cc0ef90fc5a4814b636ad971b7be1c5c5293
Lio-RP <libanr4243@gmail.com>
2022-11-26 16:34:58 +0300

    initializing The Project

---
+++ src/main/java/com/dev/app/bookstorerestapi/BookStoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.app.bookstorerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreRestApiApplication.class, args);
+    }
+
+}
