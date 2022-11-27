20b77bdf350ccf1f38989f01f950ffc83ef183d8
Lio-RP <libanr4243@gmail.com>
2022-11-27 04:36:59 +0300

    Added annotation @JsonIgnoreProperties in to Book entity

--- src/main/java/com/dev/app/bookstorerestapi/domain/Book.java
+++ src/main/java/com/dev/app/bookstorerestapi/domain/Book.java
@@ -1,5 +1,6 @@
 package com.dev.app.bookstorerestapi.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class Book extends BaseEntity {
     private String edition;
 
     @ManyToOne
+    @JsonIgnoreProperties("books")
     private Category category;
 
     @ManyToMany
